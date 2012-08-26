10982ce8dd985e78e88b96d9898e9bcfe253635f
Bruno Oliveira <bruno@abstractj.org>
2012-08-26 18:37:02 -0300

    Update copyright

--- src/test/java/org/abstractj/cuckootp/api/totp/TOTPSample.java
+++ src/test/java/org/abstractj/cuckootp/api/totp/TOTPSample.java
@@ -1,16 +1,21 @@
+/**
+ * Copyright 2012 Bruno Oliveira, and individual contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.abstractj.cuckootp.api.totp;
 
-
-/**
- Copyright (c) 2011 IETF Trust and the persons identified as
- authors of the code. All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, is permitted pursuant to, and subject to the license
- terms contained in, the Simplified BSD License set forth in Section
- 4.c of the IETF Trust's Legal Provisions Relating to IETF Documents
- (http://trustee.ietf.org/license-info).
- */
 
 import java.lang.reflect.UndeclaredThrowableException;
 import java.security.GeneralSecurityException;
