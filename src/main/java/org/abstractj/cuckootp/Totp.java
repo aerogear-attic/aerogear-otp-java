7e6f855dfa13216119e6d921c3fe46616971f917
Bruno Oliveira <bruno@abstractj.org>
2012-11-12 20:52:04 -0200

    Few stupid tests with time

--- src/main/java/org/abstractj/cuckootp/Totp.java
+++ src/main/java/org/abstractj/cuckootp/Totp.java
@@ -1,5 +1,7 @@
 package org.abstractj.cuckootp;
 
+import org.apache.commons.codec.binary.Base32;
+
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -8,6 +10,7 @@ public class Totp {
     private Clock clock;
     private Secret secret;
 
+    private static final int WINDOW = 30;
 
     public Totp() {
     }
@@ -15,10 +18,6 @@ public class Totp {
     public Totp(Secret secret, Clock clock) {
         this.clock = clock;
         this.secret = secret;
-    }
-
-    public void verify() {
-
     }
 
     public int generate() {
@@ -38,5 +37,26 @@ public class Totp {
         int binary = ((hash[offset] & 0x7f) << 24) | ((hash[offset + 1] & 0xff) << 16) | ((hash[offset + 2] & 0xff) << 8) | (hash[offset + 3] & 0xff);
 
         return binary % Digits.SIX.getValue();
+    }
+
+    public boolean verify(long code) {
+
+        Base32 codec = new Base32();
+        byte[] decodedKey = codec.decode(secret.getBytes());
+
+        // Window is used to check codes generated in the near past.
+        // You can use this value to tune how far you're willing to go.
+        int window = WINDOW;
+
+        for (int i = -window; i <= window; ++i) {
+            long hash = generate();
+
+            if (hash == code) {
+                return true;
+            }
+        }
+
+        // The validation code is invalid.
+        return false;
     }
 }
--- src/test/java/org/abstractj/cuckootp/TotpTest.java
+++ src/test/java/org/abstractj/cuckootp/TotpTest.java
@@ -2,11 +2,61 @@ package org.abstractj.cuckootp;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class TotpTest {
 
     @Test
     public void testGenerate() throws Exception {
         String keyString = "6165726f67656172";
-        System.out.println(new Totp(new Secret(keyString), new Clock()).generate());
+        int otp = new Totp(new Secret(keyString), new Clock()).generate();
+        System.out.println(otp);
+        assertEquals(6, Integer.toString(otp).length());
+    }
+
+    @Test
+    public void testOtpIsValid() throws Exception {
+        String keyString = "6165726f67656172";
+        Totp totp = new Totp(new Secret(keyString), new Clock());
+        int otp = totp.generate();
+        assertTrue("OTP is not valid", totp.verify(otp));
+    }
+
+    @Test
+    public void testOtpIsValidAfter10seconds() throws Exception {
+        String keyString = "6165726f67656172";
+        Totp totp = new Totp(new Secret(keyString), new Clock());
+        int otp = totp.generate();
+        Thread.sleep(10000L);
+        assertTrue("OTP is not valid", totp.verify(otp));
+    }
+
+    @Test
+    public void testOtpIsValidAfter20seconds() throws Exception {
+        String keyString = "6165726f67656172";
+        Totp totp = new Totp(new Secret(keyString), new Clock());
+        int otp = totp.generate();
+        Thread.sleep(20000L);
+        assertTrue("OTP is not valid", totp.verify(otp));
+    }
+
+    @Test
+    public void testOtpIsValidAfter30seconds() throws Exception {
+        String keyString = "6165726f67656172";
+        Totp totp = new Totp(new Secret(keyString), new Clock());
+        int otp = totp.generate();
+        Thread.sleep(20000L);
+        assertTrue("OTP is not valid", totp.verify(otp));
+    }
+
+    @Test
+    public void testOtpHasExpired() throws Exception {
+        String keyString = "6165726f67656172";
+        Totp totp = new Totp(new Secret(keyString), new Clock());
+        int otp = totp.generate();
+        Thread.sleep(40000L);
+        assertFalse("OTP should be invalid", totp.verify(otp));
     }
 }
