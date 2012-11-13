a4e66fc6718026bf516c519451638313517211f5
Bruno Oliveira <bruno@abstractj.org>
2012-11-12 23:07:32 -0200

    Revert "More precision to test time with Joda"
    This reverts commit f67caa185682058fe583d3a4337cff6fa08943d7.

--- src/test/java/org/abstractj/cuckootp/TotpTest.java
+++ src/test/java/org/abstractj/cuckootp/TotpTest.java
@@ -1,13 +1,6 @@
 package org.abstractj.cuckootp;
 
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeUtils;
-import org.joda.time.DateTimeZone;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Locale;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
@@ -15,31 +8,11 @@ import static org.junit.Assert.assertTrue;
 
 public class TotpTest {
 
-    private final long fixedInstant = 1587051329670L;
-    private final DateTimeZone defaultTimeZone = DateTimeZone.getDefault();
-
-    @Before
-    public void setUp() {
-        DateTimeUtils.setCurrentMillisFixed(fixedInstant);
-        DateTimeZone.setDefault(DateTimeZone.forID("Brazil/East"));
-        Locale.setDefault(Locale.forLanguageTag("en_US"));
-        System.out.println(DateTime.now());
-    }
-
-    @After
-    public void tearDown() {
-        DateTimeUtils.setCurrentMillisSystem();
-        DateTimeZone.setDefault(defaultTimeZone);
-    }
-
-    private long updateTime(int seconds) {
-        return new DateTime(fixedInstant).plusSeconds(seconds).getMillis();
-    }
-
     @Test
     public void testGenerate() throws Exception {
         String keyString = "6165726f67656172";
         int otp = new Totp(new Secret(keyString), new Clock()).generate();
+        System.out.println(otp);
         assertEquals(6, Integer.toString(otp).length());
     }
 
@@ -56,7 +29,7 @@ public class TotpTest {
         String keyString = "6165726f67656172";
         Totp totp = new Totp(new Secret(keyString), new Clock());
         int otp = totp.generate();
-        DateTimeUtils.setCurrentMillisFixed(updateTime(10));
+        Thread.sleep(10000L);
         assertTrue("OTP is not valid", totp.verify(otp));
     }
 
@@ -65,7 +38,7 @@ public class TotpTest {
         String keyString = "6165726f67656172";
         Totp totp = new Totp(new Secret(keyString), new Clock());
         int otp = totp.generate();
-        DateTimeUtils.setCurrentMillisFixed(updateTime(20));
+        Thread.sleep(20000L);
         assertTrue("OTP is not valid", totp.verify(otp));
     }
 
@@ -74,7 +47,7 @@ public class TotpTest {
         String keyString = "6165726f67656172";
         Totp totp = new Totp(new Secret(keyString), new Clock());
         int otp = totp.generate();
-        DateTimeUtils.setCurrentMillisFixed(updateTime(30));
+        Thread.sleep(20000L);
         assertTrue("OTP is not valid", totp.verify(otp));
     }
 
@@ -83,8 +56,7 @@ public class TotpTest {
         String keyString = "6165726f67656172";
         Totp totp = new Totp(new Secret(keyString), new Clock());
         int otp = totp.generate();
-        DateTimeUtils.setCurrentMillisFixed(updateTime(40));
-        System.out.println(DateTime.now());
+        Thread.sleep(40000L);
         assertFalse("OTP should be invalid", totp.verify(otp));
     }
 }
